475e6e089e7d3567630fe91601e5227f5ef22da0
Gowtham v <viji29783@gmail.com>
2025-07-08 17:52:02 +0530

    Time: 1 ms (86.32%) | Memory: 41.3 MB (20.57%) - LeetSync

--- 69-sqrtx/sqrtx.java
+++ 69-sqrtx/sqrtx.java
@@ -1,6 +1,22 @@
 class Solution {
     public int mySqrt(int x) {
-        double ans = Math.abs(Math.sqrt(x));
-        return (int)ans;
+      //  double ans = Math.abs(Math.sqrt(x));
+       // return (int)ans;
+
+        int l=1;
+        int r=x;
+        while(l<=r){
+            int m=(l+r)/2;
+            if(m>x/m){
+                r=m-1;
+            }
+            else if(m<x/m){
+                l=m+1;
+            }
+            else{
+                return m;
+            }
+        }
+        return r;
     }
 }
