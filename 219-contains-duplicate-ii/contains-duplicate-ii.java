22706d73ec5c190fdb2e33c08ba5d11f4611784d
Gowtham v <viji29783@gmail.com>
2025-07-17 17:26:11 +0530

    Time: 17 ms (97.47%) | Memory: 58.5 MB (91.98%) - LeetSync

--- 219-contains-duplicate-ii/contains-duplicate-ii.java
+++ 219-contains-duplicate-ii/contains-duplicate-ii.java
@@ -1,4 +1,4 @@
-class Solution {
+/*class Solution {
     public boolean containsNearbyDuplicate(int[] nums, int k) {
         for (int i = 0; i < nums.length; i++) {
             for (int j = i + 1; j <= i + k && j < nums.length; j++) {
@@ -7,6 +7,22 @@ class Solution {
                 }
             }
         }
+        return false;
+    }
+}*/
+class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        Set<Integer> set = new HashSet<>();
+        
+        for (int i = 0; i < nums.length; i++) {
+            if (i > k) {
+                set.remove(nums[i - k - 1]);
+            }
+            if (!set.add(nums[i])) {
+                return true;
+            }
+        }
+        
         return false;
     }
 }
