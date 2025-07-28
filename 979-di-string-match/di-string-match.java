c5ca3ca23c74501646c99b46bf66f44761cfad46
Gowtham v <viji29783@gmail.com>
2025-07-28 20:33:46 +0530

    Time: 2 ms (97.90%) | Memory: 45.6 MB (9.41%) - LeetSync

---
+++ 979-di-string-match/di-string-match.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+class Solution {
+    public int[] diStringMatch(String s) {
+        int n = s.length();
+        int[] result = new int[n + 1];
+
+        int low = 0, high = n;
+
+        for (int i = 0; i < n; i++) {
+            if (s.charAt(i) == 'I') {
+                result[i] = low++;
+            } else {
+                result[i] = high--;
+            }
+        }
+        result[n] = low; 
+        return result;
+    }
+}
